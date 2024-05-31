c9ee1398d4421ee9ae4d684f58abaadeadafb98c
frako23 <frako23@gmail.com>
2024-05-31 18:15:47 -0400

    ajusta la pagina de reporte de errores

--- src/js/pages/Bugs.js
+++ src/js/pages/Bugs.js
@@ -2,6 +2,7 @@ import React, { useContext, useEffect } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/dashboard.css";
 import { useForm, ValidationError } from "@formspree/react";
+import logo from "../../../public/logoNavBar.png";
 
 export const Bugs = () => {
   const { actions } = useContext(Context);
@@ -11,7 +12,23 @@ export const Bugs = () => {
 
   const [state, handleSubmit] = useForm("xjvnqkdv");
   if (state.succeeded) {
-    return <p>Thanks for joining!</p>;
+    return (
+      <div
+        style={{
+          paddingLeft: "26rem",
+          paddingRight: "21rem",
+        }}
+      >
+        <img src={logo} style={{ height: "14rem" }} alt="Logo de LinkedTeam" />
+        <h3>¡Gracias por tu ayuda!</h3>
+        <p>
+          Valoramos tu tiempo y tu colaboración al reportar un error en la app.
+          Tu ayuda nos permite encontrar y solucionar problemas que podrían
+          pasar desapercibidos.{" "}
+        </p>
+        <b>Gracias a tus comentarios, podemos mejorar la app para todos.</b>
+      </div>
+    );
   }
   return (
     <div className="n-container">
@@ -23,22 +40,42 @@ export const Bugs = () => {
         Aquí puedes reportarlo
       </h3>
 
-      <div style={{ paddingLeft: "7rem", paddingRight: "3rem" }}>
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="email">Email Address</label>
-          <input id="email" type="email" name="email" />
-          <ValidationError prefix="Email" field="email" errors={state.errors} />
-          <textarea id="message" name="message" />
+      <form
+        onSubmit={handleSubmit}
+        style={{ paddingLeft: "7rem", paddingRight: "3rem" }}
+      >
+        <div className="mb-3">
+          <label htmlFor="title" className="form-label">
+            <b>Titulo del error</b>
+          </label>
+          <input type="text" className="form-control" id="title" name="title" />
+          <ValidationError prefix="Text" field="title" errors={state.errors} />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="message" className="form-label">
+            <b>Detalle del error</b>
+          </label>
+          <textarea
+            style={{ height: "12rem" }}
+            className="form-control"
+            id="message"
+            name="message"
+          />
           <ValidationError
             prefix="Message"
             field="message"
             errors={state.errors}
           />
-          <button type="submit" disabled={state.submitting}>
-            Submit
-          </button>
-        </form>
-      </div>
+        </div>
+
+        <button
+          type="submit"
+          disabled={state.submitting}
+          className="btn btn-primary"
+        >
+          Enviar
+        </button>
+      </form>
     </div>
   );
 };
