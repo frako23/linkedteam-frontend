81fd91e7dd353a181df3d0b0b85e0fdb4bf1389d
frako23 <frako23@gmail.com>
2024-06-19 09:18:53 -0400

    ajuste en home

--- src/js/pages/home.js
+++ src/js/pages/home.js
@@ -185,18 +185,18 @@ export const Home = () => {
       <section className="footer">
         <h3>Información de Contacto</h3>
         <div className="box">
-          <a href="#">
+          {/* <a href="#">
             {" "}
             <i className="fa-brands fa-whatsapp"></i> +(58)0412-XXXXXX
-          </a>
+          </a> */}
           <a href="mailto:linkedteam2023@gmail.com">
             {" "}
             <i className="fas fa-envelope"></i> linkedteam2023@gmail.com
           </a>
-          <a href="#">
+          {/* <a href="#">
             {" "}
             <i className="fas fa-map"></i> Caracas, Venezuela
-          </a>
+          </a> */}
         </div>
 
         <div className="credit">
