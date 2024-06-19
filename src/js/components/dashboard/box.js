7515a32d14c45a7ce33ddb961b236857774f9d76
frako23 <frako23@gmail.com>
2024-06-19 11:31:39 -0400

    agrega monto en resumen de ventas

--- src/js/components/dashboard/box.js
+++ src/js/components/dashboard/box.js
@@ -12,56 +12,104 @@ export const Box = () => {
   return (
     <div className="main__card">
       <div className="card__profile">
-        <div className="card__inner ">
-          <p className="text-black">PROSPECTOS</p>
+        <div className="d-flex justify-content-between w-100">
+          <div className="card__inner ">
+            <p className="text-black">PROSPECTOS</p>
+          </div>
+          <p className="text-black font-weight-bold numero">
+            {
+              store.clientes.filter((index) => index.status == "Prospecto")
+                .length
+            }
+          </p>
         </div>
-        <p className="text-black font-weight-bold numero">
-          {store.clientes.filter((index) => index.status == "Prospecto").length}
-        </p>
-      </div>
-
-      <div className="card__profile">
-        <div className="card__inner">
-          <p className="text-black">CONTACTADOS</p>
-        </div>
-        <span className="text-black font-weight-bold numero">
-          {
-            store.clientes.filter((index) => index.status == "Contactado")
-              .length
-          }
+        <span>
+          {" "}
+          $
+          {store.clientes
+            .filter((cliente) => cliente.status === "Prospecto")
+            .reduce((acum, index) => acum + parseInt(index.amount), 0)}
         </span>
       </div>
 
       <div className="card__profile">
-        <div className="card__inner">
-          <p className="text-black">PRIMERA CITA</p>
+        <div className="d-flex justify-content-between w-100">
+          <div className="card__inner">
+            <p className="text-black">CONTACTADOS</p>
+          </div>
+          <p className="text-black font-weight-bold numero">
+            {
+              store.clientes.filter((index) => index.status == "Contactado")
+                .length
+            }
+          </p>
         </div>
-        <span className="text-black font-weight-bold numero">
-          {
-            store.clientes.filter((index) => index.status == "Primera Cita")
-              .length
-          }
+        <span>
+          {" "}
+          $
+          {store.clientes
+            .filter((cliente) => cliente.status === "Contactado")
+            .reduce((acum, index) => acum + parseInt(index.amount), 0)}
         </span>
       </div>
 
       <div className="card__profile">
-        <div className="card__inner">
-          <p className="text-black">NEGOCIACIÓN</p>
+        <div className="d-flex justify-content-between w-100">
+          <div className="card__inner">
+            <p className="text-black">PRIMERA CITA</p>
+          </div>
+          <span className="text-black font-weight-bold numero">
+            {
+              store.clientes.filter((index) => index.status == "Primera Cita")
+                .length
+            }
+          </span>
         </div>
-        <span className="text-black font-weight-bold numero">
-          {
-            store.clientes.filter((index) => index.status == "Negociación")
-              .length
-          }
+        <span>
+          {" "}
+          $
+          {store.clientes
+            .filter((cliente) => cliente.status === "Primera Cita")
+            .reduce((acum, index) => acum + parseInt(index.amount), 0)}
         </span>
       </div>
 
       <div className="card__profile">
-        <div className="card__inner">
-          <p className="text-black">CIERRES</p>
+        <div className="d-flex justify-content-between w-100">
+          <div className="card__inner">
+            <p className="text-black">NEGOCIACIÓN</p>
+          </div>
+          <span className="text-black font-weight-bold numero">
+            {
+              store.clientes.filter((index) => index.status == "Negociación")
+                .length
+            }
+          </span>
         </div>
-        <span className="text-black font-weight-bold numero">
-          {store.clientes.filter((index) => index.status == "Cerrado").length}
+        <span>
+          {" "}
+          $
+          {store.clientes
+            .filter((cliente) => cliente.status === "Negociación")
+            .reduce((acum, index) => acum + parseInt(index.amount), 0)}
+        </span>
+      </div>
+
+      <div className="card__profile">
+        <div className="d-flex justify-content-between w-100">
+          <div className="card__inner">
+            <p className="text-black">CIERRES</p>
+          </div>
+          <span className="text-black font-weight-bold numero">
+            {store.clientes.filter((index) => index.status == "Cerrado").length}
+          </span>
+        </div>
+        <span>
+          {" "}
+          $
+          {store.clientes
+            .filter((cliente) => cliente.status === "Cerrado")
+            .reduce((acum, index) => acum + parseInt(index.amount), 0)}
         </span>
       </div>
     </div>
