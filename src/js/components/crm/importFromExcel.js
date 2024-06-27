6bebf3b711ba0f32b53e867f33b9e7d44b751c78
Ernesto Medina <ernesto.m.medina.m@gmail.com>
2024-06-27 16:48:31 -0400

    :zap: Actualiza las opciones del file read

--- src/js/components/crm/importFromExcel.js
+++ src/js/components/crm/importFromExcel.js
@@ -29,7 +29,7 @@ export const ImportFromExcel = () => {
     reader.readAsBinaryString(e.target.files[0]);
     reader.onload = (e) => {
       const data = e.target.result;
-      const workbook = XLSX.read(data, { type: "binary" });
+      const workbook = XLSX.read(data, { type: "binary", cellDates: true });
       const sheetName = workbook.SheetNames[0];
       const sheet = workbook.Sheets[sheetName];
       const parsedData = XLSX.utils.sheet_to_json(sheet);
